10cd584ae8c805af6eb0412a5d97ac5631a57013
Erika Lopes <caluerika@gmail.com>
2021-12-17 10:00:59 -0300

    first commit

---
+++ variables.py
@@ -0,0 +1,22 @@
+a = 'Hello'
+n = "Hello"
+
+b = 20
+c = 30
+e, f, g = 1,3,4
+print('a', a)
+print('b', b)
+print('c', c)
+print('b + c =', b + c)
+
+print('e', e)
+print('f', f)
+print('g', g)
+print('n com aspas duplas', n)
+# para destacar uma letra com aspas, pode-se colocar aspas duplas
+# em volta da string, frase e destacar a palavra desejada com aspas 
+# simples e vice versa com aspas simples em volta e destaca a palavra desejada com aspas duplas
+print("A letra 'b' está destacada com aspas simples!")
+print('A letra "b" está destacada com aspas simples!')
+
+print('type', type(a))
